ee23ef7be4d19a1f40bbb815c120ec021e06fdb3
quan <1583090876@qq.com>
2018-05-29 22:50:07 +0800

    first commit

---
+++ src/app/mrquan/practice/StartThread.java
@@ -0,0 +1,45 @@
+package app.mrquan.practice;
+//开启线程的两种常见方法
+
+
+class RunThread extends Thread{
+	//方法一继承Thread类创建线程
+	@Override
+	public void run() {
+		for(int i = 0;i < 10;i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class RunRunnable implements Runnable{
+//实现Runnable接口创建线程
+	@Override
+	public void run() {
+		for(int i = 0;i < 100;i++) {
+			System.out.println("\t"+i);
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
+public class StartThread {
+	public static void main(String[] args) {
+		RunThread thread1 = new RunThread();
+		thread1.start();
+		Thread thread2 = new Thread(new RunRunnable());
+		thread2.start();
+	}
+}
